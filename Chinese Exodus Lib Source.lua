ccd0c0dadb08b7a230ac28b99889de03f28a5aab
zbfl <ngocnhi06ac5@gmail.com>
2024-07-18 15:15:54 +1200

    Update Chinese Exodus Lib Source.lua

--- Chinese Exodus Lib Source.lua
+++ Chinese Exodus Lib Source.lua
@@ -1025,7 +1025,7 @@ function library:init()
                 Position = newUDim2(.5,0,0,1);
                 ThemeColor = 'Primary Text';
                 Text = indicator.title;
-                Size = 20;
+                Size = 13;
                 Font = 2;
                 ZIndex = z+2;
                 Center = true;
