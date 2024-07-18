bbe6852307eedb09b904000843160142037c22da
zbfl <ngocnhi06ac5@gmail.com>
2024-07-18 15:19:08 +1200

    Update Chinese Exodus Lib Source.lua

--- Chinese Exodus Lib Source.lua
+++ Chinese Exodus Lib Source.lua
@@ -4739,7 +4739,7 @@ function library:CreateSettingsTab(menu)
 
     refreshConfigs()
 
-    mainSection:AddBind({text = 'Open / Close', flag = 'togglebind', nomouse = true, noindicator = true, bind = Enum.KeyCode.LCTRL, callback = function()
+    mainSection:AddBind({text = 'Open / Close', flag = 'togglebind', nomouse = true, noindicator = true, bind = Enum.KeyCode.END, callback = function()
         library:SetOpen(not library.open)
     end});
 
